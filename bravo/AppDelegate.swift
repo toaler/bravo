e9f50a36825c46565f15ba3d8d82de8e03cceec6
Brian <btoal@salesforce.com>
2018-01-27 21:33:54 -0800

    Added icon to menu bar

--- bravo/AppDelegate.swift
+++ bravo/AppDelegate.swift
@@ -1,27 +1,31 @@
-//
-//  AppDelegate.swift
-//  bravo
-//
-//  Created by Brian Toal on 1/27/18.
-//  Copyright © 2018 Brian Toal. All rights reserved.
-//
-
 import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet weak var window: NSWindow!
-
-
+    @IBOutlet weak var statusMenu: NSMenu!
+    
+    let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
+        statusItem.title = "Bravo"
+        statusItem.menu = statusMenu
+        
+        
+        
+        let icon = NSImage(named: NSImage.Name(rawValue : "statusIcon"))
+        statusItem.image = icon
+        statusItem.menu = statusMenu
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
     }
-
-
+    
+    
+    @IBAction func quitClicked(_ sender: Any) {
+        NSApplication.shared.terminate(self)
+    }
 }
 
