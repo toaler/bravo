95ef30f7c3c05e0d0c279583e79a6f01d311f49b
Brian <btoal@salesforce.com>
2018-01-27 16:01:36 -0800

    Initial Commit

---
+++ bravo/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  bravo
+//
+//  Created by Brian Toal on 1/27/18.
+//  Copyright © 2018 Brian Toal. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
